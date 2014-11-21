ba415cca43244d9d5882c4e4de0858f9902c346e
Rictas <giehle@hs-ulm.de>
2014-11-21 13:35:12 +0100

    FrameManager now renders 3d in inventory

--- src/main/java/com/rk/framely/client/renderer/WorldRendererFrameManager.java
+++ src/main/java/com/rk/framely/client/renderer/WorldRendererFrameManager.java
@@ -1,5 +1,6 @@
 package com.rk.framely.client.renderer;
 
+import com.rk.framely.block.BlockFrameManager;
 import com.rk.framely.proxy.ClientProxy;
 import com.rk.framely.proxy.CommonProxy;
 import com.rk.framely.tileentity.TileEntityEngine;
@@ -14,6 +15,7 @@ import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.common.util.ForgeDirection;
 import org.lwjgl.opengl.GL11;
 
 import java.util.ArrayList;
@@ -23,7 +25,56 @@ public class WorldRendererFrameManager implements ISimpleBlockRenderingHandler {
 
     @Override
     public void renderInventoryBlock(Block block, int metadata, int modelId, RenderBlocks renderer) {
+        if(block instanceof BlockFrameManager) {
+            Tessellator tessellator = Tessellator.instance;
+            float p_147800_3_ = 1.0f;
 
+            block.setBlockBoundsForItemRender();
+            renderer.setRenderBoundsFromBlock(block);
+            GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
+            GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
+            tessellator.startDrawingQuads();
+            tessellator.setNormal(0.0F, -1.0F, 0.0F);
+            renderer.renderFaceYNeg(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 0, metadata));
+            tessellator.draw();
+
+            if (renderer.useInventoryTint)
+            {
+                int k = block.getRenderColor(metadata);
+                float f2 = (float)(k >> 16 & 255) / 255.0F;
+                float f3 = (float)(k >> 8 & 255) / 255.0F;
+                float f4 = (float)(k & 255) / 255.0F;
+                GL11.glColor4f(f2 * p_147800_3_, f3 * p_147800_3_, f4 * p_147800_3_, 1.0F);
+            }
+
+            tessellator.startDrawingQuads();
+            tessellator.setNormal(0.0F, 1.0F, 0.0F);
+            renderer.renderFaceYPos(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 1, metadata));
+            tessellator.draw();
+
+            if (renderer.useInventoryTint)
+            {
+                GL11.glColor4f(p_147800_3_, p_147800_3_, p_147800_3_, 1.0F);
+            }
+
+            tessellator.startDrawingQuads();
+            tessellator.setNormal(0.0F, 0.0F, -1.0F);
+            renderer.renderFaceZNeg(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 2, metadata));
+            tessellator.draw();
+            tessellator.startDrawingQuads();
+            tessellator.setNormal(0.0F, 0.0F, 1.0F);
+            renderer.renderFaceZPos(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 3, metadata));
+            tessellator.draw();
+            tessellator.startDrawingQuads();
+            tessellator.setNormal(-1.0F, 0.0F, 0.0F);
+            renderer.renderFaceXNeg(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 4, metadata));
+            tessellator.draw();
+            tessellator.startDrawingQuads();
+            tessellator.setNormal(1.0F, 0.0F, 0.0F);
+            renderer.renderFaceXPos(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 5, metadata));
+            tessellator.draw();
+            GL11.glTranslatef(0.5F, 0.5F, 0.5F);
+        }
     }
 
     @Override
@@ -37,7 +88,7 @@ public class WorldRendererFrameManager implements ISimpleBlockRenderingHandler {
 
     @Override
     public boolean shouldRender3DInInventory(int modelId) {
-        return false;
+        return true;
     }
 
     @Override
